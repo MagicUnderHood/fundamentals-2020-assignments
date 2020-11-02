91d912e32d2b64fb19f8a0a97653b4735aa0c55a
tanya <tanya.orlova17@gmail.com>
2020-11-02 22:14:38 +0300

    Задание 3

--- app/src/main/java/com/android/academy/fundamentals/MainActivity.kt
+++ app/src/main/java/com/android/academy/fundamentals/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.android.academy.fundamentals
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.android.academy.fundamentals.SecondActivity.Companion.TRANSMITTED_BOOLEAN
+import com.android.academy.fundamentals.SecondActivity.Companion.TRANSMITTED_INT
+import com.android.academy.fundamentals.SecondActivity.Companion.TRANSMITTED_STRING
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView: TextView = findViewById(R.id.first_activity_text_view)
+        textView.setOnClickListener { moveToNextScreen() }
+    }
+
+    private fun moveToNextScreen() {
+        val intent = Intent(this, SecondActivity::class.java)
+
+        val transmittedString = "string to transmit"
+        intent.putExtra(TRANSMITTED_STRING, transmittedString)
+
+        val transmittedInt = 12
+        intent.putExtra(TRANSMITTED_INT, transmittedInt)
+
+        val transmittedBoolean = false
+        intent.putExtra(TRANSMITTED_BOOLEAN, transmittedBoolean)
+
+        startActivity(intent)
     }
 }
